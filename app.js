363a76a308a3ff7d29c86767c333d06e8a3a3ba2
indamutsa <arsichizy@gmail.com>
2019-04-14 23:59:16 +0200

    Node passport

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//DB Config
+const db = require('./config/keys').MongoURI;
+
+//Connect to mongo
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err))
+
+
+app.use(expressLayouts);
+app.set('view engine', 'ejs')
+
+app.use('/', require('./routes/index.js'))
+app.use('/users', require('./routes/users.js'))
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
